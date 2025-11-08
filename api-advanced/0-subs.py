e824ed8fabe370a26cc6b4e3ec2b46c65ee5dba0
ydejene <y.dejene@alustudent.com>
2025-11-08 14:42:07 +0300

    subscribers count module added

---
+++ api-advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Get number of subscribers for a subreddit. If invalid return 0."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return total number of subscribers for a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "script:com.subscribers-count.myredditapp:v1.0 (by /u/yonasdejene)"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    data = response.json().get("data", {})
+    return data.get("subscribers", 0)
